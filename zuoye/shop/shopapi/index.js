c39d7435b8e1fa087dd5f4d029b42b9ba5a77e31
liu shuo <liushuo@qq.com>
2019-11-14 21:23:05 +0800

    end

--- zuoye/shop/shopapi/index.js
+++ zuoye/shop/shopapi/index.js
@@ -9,6 +9,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // 引入md5
 const md5 = require('md5')
 const mi = 'sddas/*/-5456456!@../*'
+// 引入jsonwebtoken
+const jsonwebtoken = require('jsonwebtoken')
 // 1 引入 mysql
 const mysql = require('mysql')
 // 2 创建一个新的连接对象
@@ -137,6 +139,25 @@ app.post('/api/v1/register', (req, res) => {
     let password = req.body.password
     let regtime = Date.parse(new Date()).toString().substr(0, 10);
 
+    let mobRef = /^1([38][0-9]|4[579]|5[0-3,5-9]|6[6]|7[0135678]|9[89])\d{8}$/
+    let passRef = /^[\w_-]{6,16}$/
+
+    if (!mobRef.test(mobile)) {
+        res.json({
+            "ok": 0,
+            "error": "手机号码格式不正确"
+        })
+        return
+    }
+
+    if (!passRef.test(password)) {
+        res.json({
+            "ok": 0,
+            "error": "密码必须是6-16位字母数字_-组成！"
+        })
+        return
+    }
+
     let sql = `select count(*) m from shop_users where mobile = ?`
 
     // 查询手机号是否已经添加过
@@ -155,10 +176,11 @@ app.post('/api/v1/register', (req, res) => {
                 })
                 return
             } else {
+
                 let sql2 = 'insert into shop_users set ?'
                 let data = {
                     mobile,
-                    password: password,
+                    password: md5(password + mi),
                     regtime: regtime
                 }
                 db.query(sql2, data, (err, data) => {
@@ -185,6 +207,72 @@ app.post('/api/v1/register', (req, res) => {
 // 启动服务器
 // 登录接口
 app.post('/api/v1/login', (req, res) => {
+    let mobile = req.body.mobile;
+    let password = req.body.password
+
+
+    let mobRef = /^1([38][0-9]|4[579]|5[0-3,5-9]|6[6]|7[0135678]|9[89])\d{8}$/
+    let passRef = /^[\w_-]{6,16}$/
+    // 判断手机号
+    if (!mobRef.test(mobile)) {
+        res.json({
+            "ok": 0,
+            "error": "手机号码格式不正确"
+        })
+        return
+    }
+    // 判断密码
+    if (!passRef.test(password)) {
+        res.json({
+            "ok": 0,
+            "error": "密码必须是6-16位字母数字_-组成！"
+        })
+        return
+    }
+    // 判断手机号是否存在
+    let sql = 'select * from shop_users where mobile =?'
+
+    db.query(sql, mobile, (err, data) => {
+        if (err) {
+            res.json({
+                "ok": 0,
+                "error": err
+            })
+
+        } else {
+            //   "data": []
+            // res.json({
+            //     'data': data
+            // })
+            if (data.length > 0) {
+                // 判断密码
+                if (data[0].password === md5(password + mi)) {
+                    let jwt = jsonwebtoken.sign({
+                        id: data[0].id
+                    }, mi, { expiresIn: 60 * 60 * 24 * 30 * 6 }) //半年， 单位秒
+                    res.json({
+                        "ok": 1,
+                        'data': {
+                            "token": jwt
+                        }
+                    })
+                    return
+                } else {
+                    res.json({
+                        "ok": 0,
+                        "error": "密码错误"
+                    })
+                }
+            } else {
+                res.json({
+                    "ok": 0,
+                    "error": "手机号不存在"
+                })
+            }
+          
+        }
+
+    })
 
     // res.json({
     //     // "data":0
