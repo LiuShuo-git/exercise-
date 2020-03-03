f3754d7e9571a3e0c59694ca1cdc876662c9836c
liu shuo <liushuo@qq.com>
2020-03-03 23:52:34 +0800

    ts

---
+++ zuoye/vue-lx/index.ts
@@ -0,0 +1,8 @@
+console.log('Hello Ts');
+console.log('hhh');
+
+let cc= function(){
+   console.log('aaa');
+   
+}
+cc()
---
+++ zuoye/vue-lx/js/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log('Hello Ts');
+console.log('hhh');
+var cc = function () {
+    console.log('aaa');
+};
+cc();
