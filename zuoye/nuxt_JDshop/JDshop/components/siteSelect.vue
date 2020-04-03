201be504bcdc9689801d55780a315b67d853f6f4
LiuS <ls526666@163.com>
2020-04-03 14:53:01 +0800

    end

---
+++ zuoye/nuxt_JDshop/JDshop/components/siteSelect.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="siteSelect">
+    <!-- 请选择 -->
+    <div class="siteSelect-top">{{issite}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      issite: "请选择",
+      current: 0,
+      data: [
+        {
+          title: "北京",
+          children: [
+            {
+              title: "朝阳区",
+              children: [
+                {
+                  title: "八里庄"
+                },
+                {
+                  title: "东坝乡"
+                },
+                {
+                  title: "北苑"
+                }
+              ]
+            },
+            {
+              title: "海淀区",
+              children: [
+                {
+                  title: "中关村"
+                },
+                {
+                  title: "知春路"
+                },
+                {
+                  title: "牡丹园"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          title: "河北省",
+          children: [
+            {
+              title: "廊坊市",
+              children: [
+                {
+                  title: "广阳区"
+                },
+                {
+                  title: "安次区"
+                },
+                {
+                  title: "燕郊"
+                }
+              ]
+            },
+            {
+              title: "石家庄市",
+              children: [
+                {
+                  title: "中关村"
+                },
+                {
+                  title: "知春路"
+                },
+                {
+                  title: "牡丹园"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ zuoye/nuxt_JDshop/JDshop/pages/site.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="site">
+    <siteSelect />
+  </div>
+</template>
+
+<script>
+import siteSelect from "~/components/siteSelect.vue";
+export default {
+  components: {
+    siteSelect
+  }
+};
+</script>
+
+<style>
+</style>
